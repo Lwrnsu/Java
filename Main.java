00eff1af4f5b4d4190d752d359a1876e4d369d94
Lwrnsu <jaylawrencedeluna.2007@gmail.com>
2025-10-18 11:43:44 +0800

    Cleaned Main.java File

--- Main.java
+++ Main.java
@@ -6,17 +6,5 @@ public class Main {
 
   public static void main(String[] args) {
    
-    Scanner scanner = new Scanner(System.in);
-    Inventory inventory = new Inventory();
-
-    Item item1 = new Item("Apples", 20);
-    Item item2 = new Item("Bananas", 10);
-
-    inventory.addItem(item1);
-    inventory.addItem(item2);
-
-    inventory.displayItem();
-
-    scanner.close();
-  }
+    
 }
