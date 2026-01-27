ed1accb0517e232d546841ff25632d742b67c541
Lwrnsu <jaylawrencedeluna.2007@gmail.com>
2026-01-27 10:03:03 +0800

    Day 1 - DONE

--- One.java
+++ One.java
@@ -1,5 +1,21 @@
+//January 27, 2026.
+
+import java.util.Scanner;
+
 public class One {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+
+    public static String sentence(String name, int age) {
+        String sentence = "Hello " + name + ", you will be " + age + " next year.";
+        return sentence;
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.print("What is  your age? ");
+        int age = scanner.nextInt();
+        System.out.print(sentence(name, age));
+    }
+
 }
