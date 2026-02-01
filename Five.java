d8c9ca966548cfbfe120dd7fa76d680eed7cbb83
Lwrnsu <jaylawrencedeluna.2007@gmail.com>
2026-02-01 10:55:22 +0800

    Day 6 - Initialized

---
+++ Five.java
@@ -0,0 +1,19 @@
+/*
+
+    February 1, 2026
+    Day 6 of Java - OOP Advanced I
+    Lesson tackled: Inheritance, Composition, Interface, Abstract, and Dependency Injection.
+
+    Task: 
+    1. You're designing a Payment System:
+    2. There are different payment methods: Cash, GCash, CreditCard.
+    3. All payment methods can process payment (interface rule).
+    4. Some methods share common behavior, like validating amount (abstract class).
+    5. PaymentProcessor uses dependency injection to receive any payment method.
+    Bonus: Add a User class that has a payment method (composition).
+
+*/
+
+public class Five {
+    
+}
